bdbd5142b4cc57560835e8f73dfdbc274cc6cc0f
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-08-10 20:43:27 +0200

    Fix bug EnvLoader

--- src/Config/EnvLoader.php
+++ src/Config/EnvLoader.php
@@ -5,7 +5,11 @@ namespace Querychan\Config;
 use Dotenv\Dotenv;
 
 class EnvLoader {
-    public static function load(string $path = __DIR__ . '/../../') {
+    public static function load() {
+        $path = __DIR__ . '/../../';
+        if(!file_exists($path)) {
+            $path = '/../../../../../../';
+        }
         $dotenv = Dotenv::createImmutable($path);
         $dotenv->load();
     }
