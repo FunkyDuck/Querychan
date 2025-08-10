9e213af295a25ff7c8eeea9da9f654a94888c88f
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-08-10 20:46:34 +0200

    Fix bug EnvLoader 1.2

--- src/Config/EnvLoader.php
+++ src/Config/EnvLoader.php
@@ -7,7 +7,7 @@ use Dotenv\Dotenv;
 class EnvLoader {
     public static function load() {
         $path = __DIR__ . '/../../';
-        if(!file_exists($path)) {
+        if(!file_exists($path . '.env')) {
             $path = '/../../../../../';
         }
         $dotenv = Dotenv::createImmutable($path);
