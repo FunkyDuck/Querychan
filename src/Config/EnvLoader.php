d063f07024f23ca078a85a6f7e0f8d143e75fffc
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-08-10 20:54:46 +0200

    Fix bug EnvLoader 1.5

--- src/Config/EnvLoader.php
+++ src/Config/EnvLoader.php
@@ -6,8 +6,7 @@ use Dotenv\Dotenv;
 
 class EnvLoader {
     public static function load() {
-        $path = getcwd() . DIRECTORY_SEPARATOR . '.env';
-        $dotenv = Dotenv::createImmutable($path);
+        $dotenv = Dotenv::createImmutable(getcwd());
         $dotenv->load();
     }
 }
